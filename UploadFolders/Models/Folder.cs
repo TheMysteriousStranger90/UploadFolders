1cc9171003ddf5b0d8049af2be0e42db331b94d6
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2023-09-21 12:26:19 +0300

    add models...

---
+++ UploadFolders/Models/Folder.cs
@@ -0,0 +1,10 @@
+﻿namespace UploadFolders.Models;
+
+public class Folder
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int? ParentId { get; set; }
+    public Folder Parent { get; set; }
+    public List<Folder> Children { get; set; }
+}
---
+++ UploadFolders/Models/UploadFolder.cs
@@ -0,0 +1,7 @@
+﻿namespace UploadFolders.Models;
+
+public class UploadFolder
+{
+    public string? Name { get; set; }
+    public string? Path { get; set; }
+}
